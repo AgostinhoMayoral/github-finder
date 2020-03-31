68164d93e2c30ce42c4a2dded8eb73648cafb657
AgostinhoMayoral <agostinho.guimaraes@fatec.sp.gov.br>
2020-03-31 02:36:30 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+
+  render(){
+    const name = 'Agostinho Mayoral';
+    const loading = false;
+    const showName = false;
+    return (
+      <div className="App">
+        {loading ? (<h4>Loading...</h4>) : (<h1>Hello {showName && name}</h1>)}
+      </div>
+    );
+
+  }
+}
+
+export default App;
