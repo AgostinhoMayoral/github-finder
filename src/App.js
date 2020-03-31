c3c13482f9a3bc9ff851551316aeea75a059ba5e
AgostinhoMayoral <agostinho.guimaraes@fatec.sp.gov.br>
2020-03-31 11:55:46 -0400

    Update App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   
 
   render(){
     const name = 'Agostinho Mayoral';
     const loading = false;
-    const showName = false;
+    const showName = true;
     return (
       <div className="App">
         {loading ? (<h4>Loading...</h4>) : (<h1>Hello {showName && name}</h1>)}
